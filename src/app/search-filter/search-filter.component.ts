96e5c8708facdd9754e25baacfa210be07d225cf
samgilmore <30483214+samgilmore@users.noreply.github.com>
2022-06-18 03:11:34 -0400

    Added bare search filter dropdown menu

--- src/app/search-filter/search-filter.component.ts
+++ src/app/search-filter/search-filter.component.ts
@@ -1,16 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-search-filter',
   templateUrl: './search-filter.component.html',
-  styleUrls: ['./search-filter.component.scss']
+  styleUrls: ['./search-filter.component.scss'],
 })
 export class SearchFilterComponent implements OnInit {
+
+  dropdownList: { item_id: string; item_text: string; }[];
+  dropdownSettings: IDropdownSettings = {};
+  selectedItems: string[] = [];
 
   constructor() { 
   }
 
   ngOnInit(): void {
+    this.dropdownList = [
+      { item_id: "BTC", item_text: 'Bitcoin (BTC)' },
+      { item_id: "ETH", item_text: 'Ethereum (ETH)' },
+      { item_id: "XRP", item_text: 'Ripple (XRP)' },
+      { item_id: "LTC", item_text: 'Litecoin (LTC)' },
+      { item_id: "BCH", item_text: 'Bitcoin Cash (BCH)' },
+      { item_id: "XLM", item_text: 'Stellar (XLM)' }
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'Unselect All',
+      allowSearchFilter: true
+    }
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+    this.selectedItems.push(item.item_id);
+  }
+
+  onItemDeSelect(item: any) {
+    let index = this.selectedItems.indexOf(item.item_id);
+    console.log("Removing: " + this.selectedItems[index]);
+
+    if (index !== -1) {
+      this.selectedItems.splice(index, 1);
+    }
+  }
+
+  onSelectAll() {
+    this.selectedItems = ["BTC","ETH","XRP","LTC","BCH","XLM"];
+  }
+
+  onDeSelectAll() {
+    this.selectedItems = [];
   }
 
 }
