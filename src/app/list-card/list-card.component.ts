a202378e683487d81cd4c55f8f48f99af4a93e7a
samgilmore <30483214+samgilmore@users.noreply.github.com>
2022-08-15 15:51:44 -0400

    Fixed graph formatting

--- src/app/list-card/list-card.component.ts
+++ src/app/list-card/list-card.component.ts
@@ -18,7 +18,8 @@ import { trigger, transition, animate, style } from '@angular/animations';
 export class ListCardComponent implements OnInit {
 
   @Input() entry: any;
-  @Input() chart_data: any;
+  @Input() fullChartData: any;
+  singleChartData: any;
   name: string;
   price: string;
   imgSrc: string;
@@ -47,5 +48,28 @@ export class ListCardComponent implements OnInit {
       this.changeDay = this.changeDay.substring(1);
       this.changeDayPositive = false;
     }
+
+    console.log(this.fullChartData);
+    switch (this.name) {
+      case "BTC":
+        this.singleChartData = this.fullChartData[0];
+        break;
+      case "ETH":
+        this.singleChartData = this.fullChartData[1];
+        break;
+      case "XRP":
+        this.singleChartData = this.fullChartData[2];
+        break;
+      case "LTC":
+        this.singleChartData = this.fullChartData[3];
+        break;
+      case "BCH":
+        this.singleChartData = this.fullChartData[4];
+        break;
+      case "XLM":
+        this.singleChartData = this.fullChartData[5];
+        break;
+    }
+
   }
 }
